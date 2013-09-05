68393c6e5828032b0b8acdc812629e6a8bd88033
Timothy A. Graupmann <tgraupmann@gmail.com>
2013-09-05 13:20:49 -0700

    Preparing submission

---
+++ Corona/Submission/ouya/docs/ouya/index.markdown
@@ -0,0 +1,66 @@
+# ouya.*
+
+> --------------------- ------------------------------------------------------------------------------------------
+> __Type__              [library][api.type.library]
+> __Revision__          [REVISION_LABEL](REVISION_URL)
+> __Keywords__          ouya, store, gameNetwork
+> __Sample code__       
+> __See also__          [Marketplace](http://www.coronalabs.com/store/plugin)
+> __Availability__      Starter, Pro, Enterprise
+> --------------------- ------------------------------------------------------------------------------------------
+
+## Overview
+
+
+## Platforms
+
+Ouya is only supported on Android.
+
+## Syntax
+
+	local ouya = require "ouya"
+
+## Functions
+
+#### [ouya.function1()][plugin.ouya.nameOfFunction1MarkdownFile]
+
+#### [ouya.function2()][plugin.ouya.nameOfFunction2MarkdownFile]
+
+## Project Settings
+
+### SDK
+
+When you build using the Corona Simulator, the server automatically takes care of integrating the plugin into your project. 
+
+All you need to do is add an entry into a `plugins` table of your `build.settings`. The following is an example of a minimal `build.settings` file:
+
+``````
+settings =
+{
+	plugins =
+	{
+		["plugin.ouya"] =
+		{
+			publisherId = "tv.ouya"
+		},
+	},		
+}
+``````
+
+### Enterprise
+
+The required AndroidManifest.xml settings go here...
+
+## Sample Code
+
+Provide links to sample projects here...
+Expected to be hosted by Ouya's website or a 3rd party website such as GitHub.
+
+## Support
+
+More support is available from the inneractive team:
+
+* [E-mail](mailto:tim@tagenigma.com)
+* [Forum](http://forums.ouya.tv/categories/corona-on-ouya)
+* [Docs](https://devs.ouya.tv/developers/docs/corona)
+* [Plugin Publisher](http://www.ouya.tv)
---
+++ Corona/Submission/ouya/samples/InAppPurchasesPlugin/Corona/plugin_ouya.lua
@@ -0,0 +1,109 @@
+-- Copyright (C) 2012, 2013 OUYA, Inc.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-----------------------------------------------------------------------------------------
+--
+-- plugin_ouya.lua
+--
+-----------------------------------------------------------------------------------------
+
+
+-- This script is called when require("plugin.ouya") is called.
+print "plugin.ouya is loading plugin_ouya";
+
+-- Create the Ouya Lua library object.
+local library = require("CoronaLibrary")
+local plugin_ouya = library:new{ name = "ouya", publisherId = "tv.ouya" }
+
+plugin_ouya.developerId = "310a8f51-4d6e-4ae5-bda0-b93878e5f5d0";
+
+plugin_ouya.initialized = false;
+
+plugin_ouya.initialize = function ()
+	if ouyaSDK == nil then
+		print "ouyaSDK named java functions are not initialized";
+		return;
+	end
+	if plugin_ouya.initialized == false then
+			plugin_ouya.ouyaSetDeveloperId(plugin_ouya.developerId);
+			print "plugin_ouya is initialized";
+	end
+end
+
+-- Set IAP Developer ID
+plugin_ouya.ouyaSetDeveloperId = function(developerId)
+	if ouyaSDK == nil then
+		print "ouyaSDK named java functions are not initialized";
+		return;
+	end
+
+	if plugin_ouya.initialized == true then
+		return;
+	end
+	
+	print ("plugin_ouya.ouyaSetDeveloperId (" .. developerId .. ")");
+	ouyaSDK.ouyaSetDeveloperId(developerId);
+	plugin_ouya.initialized = true;
+end
+
+-- Invoke IAP Fetch Gamer UUID and provide callbacks
+plugin_ouya.asyncLuaOuyaFetchGamerUUID = function(onSuccess, onFailure, onCancel)
+	if ouyaSDK == nil then
+		print "ouyaSDK named java functions are not initialized";
+		return;
+	end
+	plugin_ouya.initialize();
+	
+	print ("plugin_ouya.asyncLuaOuyaFetchGamerUUID");
+	ouyaSDK.asyncLuaOuyaFetchGamerUUID(onSuccess, onFailure, onCancel);
+end
+
+-- Invoke IAP Request Products, provide callbacks, and a table of product identifiers
+plugin_ouya.asyncLuaOuyaRequestProducts = function(onSuccess, onFailure, onCancel, products)
+	if ouyaSDK == nil then
+		print "ouyaSDK named java functions are not initialized";
+		return;
+	end
+	plugin_ouya.initialize();
+	
+	print ("plugin_ouya.asyncLuaOuyaRequestProducts");
+	ouyaSDK.asyncLuaOuyaRequestProducts(onSuccess, onFailure, onCancel, products);	
+end
+
+-- Invoke IAP Request Purchase, provide callbacks, and purchasable identifier
+plugin_ouya.asyncLuaOuyaRequestPurchase = function(onSuccess, onFailure, onCancel, purchasable)
+	if ouyaSDK == nil then
+		print "ouyaSDK named java functions are not initialized";
+		return;
+	end
+	plugin_ouya.initialize();
+	
+	print ("plugin_ouya.asyncLuaOuyaRequestPurchase (" .. purchasable .. ")");
+	ouyaSDK.asyncLuaOuyaRequestPurchase(onSuccess, onFailure, onCancel, purchasable);
+end
+
+-- Invoke IAP Request Receipts and provide callbacks
+plugin_ouya.asyncLuaOuyaRequestReceipts = function(onSuccess, onFailure, onCancel)
+	if ouyaSDK == nil then
+		print "ouyaSDK named java functions are not initialized";
+		return;
+	end
+	plugin_ouya.initialize();
+	
+	print ("plugin_ouya.asyncLuaOuyaRequestReceipts");
+	ouyaSDK.asyncLuaOuyaRequestReceipts(onSuccess, onFailure, onCancel)
+end
+
+-- Return the Ouya library from the require() 
+return plugin_ouya
