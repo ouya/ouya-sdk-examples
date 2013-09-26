45af582597d2d495d1779a4d682168c98ab20942
Tim Graupmann <tgraupmann@gmail.com>
2013-09-25 22:50:41 -0700

    Easy build script for Mac

---
+++ Android/InAppPurchasesNative/easy_build.sh
@@ -0,0 +1,7 @@
+NDK_PATH=~/NVPACK/android-ndk-r8e
+export NDK_PATH
+
+NDK_MODULE_PATH=~/NVPACK/TDK_Samples/tegra_android_native_samples_v10p12/libs/jni/
+export NDK_MODULE_PATH
+
+$NDK_PATH/ndk-build
