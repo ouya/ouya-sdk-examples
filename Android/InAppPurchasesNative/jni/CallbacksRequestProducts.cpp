8ad130b6d2adb4bbf870d0a82c481c45b741323b
Timothy Graupmann <tgraupmann@gmail.com>
2013-09-25 19:55:29 -0700

    Adding JSON parsing

--- Android/InAppPurchasesNative/jni/CallbacksRequestProducts.cpp
+++ Android/InAppPurchasesNative/jni/CallbacksRequestProducts.cpp
@@ -1,10 +1,13 @@
 #include "CallbacksRequestProducts.h"
 #include "CallbackSingleton.h"
+#include "JSON.h"
 
 #include <stdio.h>
 #include <android/log.h>
 #include <nv_and_util/nv_native_app_glue.h>
 #include <nv_egl_util/nv_egl_util.h>
+
+#include <iostream>
 
 #define APP_NAME "inapppurchasesnative_CallbacksRequestProducts"
 
@@ -21,10 +24,55 @@
 void CallbacksRequestProducts::OnSuccess(const char* jsonData)
 {
 	char buffer[1024];
-	sprintf(buffer, "CallbacksRequestProducts::OnSuccess:  %s", jsonData);
+	sprintf(buffer, "CallbacksRequestProducts::OnSuccess:  %s\r\n", jsonData);
 	LOGI(buffer);
 
-	LOGI("*******************");
+	LOGI("Parsing JSON Data");
+
+	// Parse example data
+	JSONValue* value = JSON::Parse(jsonData);
+
+	if (value == NULL)
+	{
+		LOGI("Parsing JSON Failed");
+		return;
+	}
+
+	if (!value->IsArray())
+	{
+		LOGI("Parsing JSON Failed: Not an array");
+		return;
+	}
+
+	// Retrieve the main object
+	JSONArray data = value->AsArray();
+
+	for (unsigned int i = 0; i < data.size(); i++)
+	{
+		if (!data[i]->IsObject())
+		{
+			LOGI("Parsing JSON Failed: Not an object item");
+			continue;
+		}
+		JSONObject item = data[i]->AsObject();
+		if (item.find(L"identifier") == item.end())
+		{
+			LOGI("Parsing JSON Failed: Can't find identifier");
+			continue;
+		}
+		if (item[L"identifier"]->IsString())
+		{
+			const wchar_t* wstr = item[L"identifier"]->AsString().c_str();
+
+			///*
+			//char id[512];
+			//std::wcsrtombs(id, &wstr, 512, 0);
+			//sprintf(id, "id=%s\r\n", str);
+			//LOGI(id);
+			//*/
+		}
+	}
+
 	LOGI("*******************");
 	LOGI("*******************");
 	LOGI("*******************");
