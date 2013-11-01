c20369dcd652b3ec652d093da655cd0ca3a59fa8
Timothy A. Graupmann <tgraupmann@gmail.com>
2013-11-01 12:08:44 -0700

    Create external storage example project

---
+++ Android/ExternalStorageExample/src/tv/ouya/sdk/android/example/externalstorageexample/MainActivity.java
@@ -0,0 +1,15 @@
+package tv.ouya.sdk.android.example.externalstorageexample;
+
+import android.os.Bundle;
+import android.app.Activity;
+import tv.ouya.console.api.OuyaController;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
