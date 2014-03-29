0657e0e1cbfda9c60e144bbddae119459b214a5e
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-03-28 22:51:50 -0700

    Added debugging

--- Android/AndroidVirtualController/src/tv/ouya/examples/android/virtualcontroller/OuyaActivity.java
+++ Android/AndroidVirtualController/src/tv/ouya/examples/android/virtualcontroller/OuyaActivity.java
@@ -56,6 +56,9 @@ public class OuyaActivity extends Activity {
 	@Override
 	public boolean dispatchGenericMotionEvent(MotionEvent motionEvent) {
 		
+		Log.i(TAG, android.os.Build.MODEL + " : dispatchGenericMotionEvent");
+		//debugMotionEvent(motionEvent);
+		
 		/*
 		if (true) {
 			return true;
@@ -124,8 +127,10 @@ public class OuyaActivity extends Activity {
 	    		motionEvent.getPointerCoords(0, pointerCoords[0]);
 	    		
 			    for (int i = 0; i < axises.size(); ++i) {
-			    	AxisRemap axis = axises.get(i);		    	
-		    		pointerCoords[0].setAxisValue(axis.mDestinationAxis, motionEvent.getAxisValue(axis.mSourceAxis));		    	
+			    	AxisRemap axis = axises.get(i);
+			    	float val = motionEvent.getAxisValue(axis.mSourceAxis);
+			    	Log.i(TAG, "Remap "+debugGetAxisName(axis.mSourceAxis) + " to "+ debugGetAxisName(axis.mDestinationAxis) + "val=" + val);
+		    		pointerCoords[0].setAxisValue(axis.mDestinationAxis, val);		    	
 			    }
 			    
 			    long downTime = motionEvent.getDownTime();
@@ -150,9 +155,64 @@ public class OuyaActivity extends Activity {
 		    }
 	    }
 		
-		///*
-	    
-	    float lsX = motionEvent.getAxisValue(OuyaController.AXIS_LS_X);
+		return super.dispatchGenericMotionEvent(motionEvent);
+	}
+	
+	private String debugGetAxisName(int axis) {
+		SparseArray<String> names = new SparseArray<String>();
+        names.append(MotionEvent.AXIS_X, "AXIS_X");
+        names.append(MotionEvent.AXIS_Y, "AXIS_Y");
+        names.append(MotionEvent.AXIS_PRESSURE, "AXIS_PRESSURE");
+        names.append(MotionEvent.AXIS_SIZE, "AXIS_SIZE");
+        names.append(MotionEvent.AXIS_TOUCH_MAJOR, "AXIS_TOUCH_MAJOR");
+        names.append(MotionEvent.AXIS_TOUCH_MINOR, "AXIS_TOUCH_MINOR");
+        names.append(MotionEvent.AXIS_TOOL_MAJOR, "AXIS_TOOL_MAJOR");
+        names.append(MotionEvent.AXIS_TOOL_MINOR, "AXIS_TOOL_MINOR");
+        names.append(MotionEvent.AXIS_ORIENTATION, "AXIS_ORIENTATION");
+        names.append(MotionEvent.AXIS_VSCROLL, "AXIS_VSCROLL");
+        names.append(MotionEvent.AXIS_HSCROLL, "AXIS_HSCROLL");
+        names.append(MotionEvent.AXIS_Z, "AXIS_Z");
+        names.append(MotionEvent.AXIS_RX, "AXIS_RX");
+        names.append(MotionEvent.AXIS_RY, "AXIS_RY");
+        names.append(MotionEvent.AXIS_RZ, "AXIS_RZ");
+        names.append(MotionEvent.AXIS_HAT_X, "AXIS_HAT_X");
+        names.append(MotionEvent.AXIS_HAT_Y, "AXIS_HAT_Y");
+        names.append(MotionEvent.AXIS_LTRIGGER, "AXIS_LTRIGGER");
+        names.append(MotionEvent.AXIS_RTRIGGER, "AXIS_RTRIGGER");
+        names.append(MotionEvent.AXIS_THROTTLE, "AXIS_THROTTLE");
+        names.append(MotionEvent.AXIS_RUDDER, "AXIS_RUDDER");
+        names.append(MotionEvent.AXIS_WHEEL, "AXIS_WHEEL");
+        names.append(MotionEvent.AXIS_GAS, "AXIS_GAS");
+        names.append(MotionEvent.AXIS_BRAKE, "AXIS_BRAKE");
+        names.append(MotionEvent.AXIS_DISTANCE, "AXIS_DISTANCE");
+        names.append(MotionEvent.AXIS_TILT, "AXIS_TILT");
+        names.append(MotionEvent.AXIS_GENERIC_1, "AXIS_GENERIC_1");
+        names.append(MotionEvent.AXIS_GENERIC_2, "AXIS_GENERIC_2");
+        names.append(MotionEvent.AXIS_GENERIC_3, "AXIS_GENERIC_3");
+        names.append(MotionEvent.AXIS_GENERIC_4, "AXIS_GENERIC_4");
+        names.append(MotionEvent.AXIS_GENERIC_5, "AXIS_GENERIC_5");
+        names.append(MotionEvent.AXIS_GENERIC_6, "AXIS_GENERIC_6");
+        names.append(MotionEvent.AXIS_GENERIC_7, "AXIS_GENERIC_7");
+        names.append(MotionEvent.AXIS_GENERIC_8, "AXIS_GENERIC_8");
+        names.append(MotionEvent.AXIS_GENERIC_9, "AXIS_GENERIC_9");
+        names.append(MotionEvent.AXIS_GENERIC_10, "AXIS_GENERIC_10");
+        names.append(MotionEvent.AXIS_GENERIC_11, "AXIS_GENERIC_11");
+        names.append(MotionEvent.AXIS_GENERIC_12, "AXIS_GENERIC_12");
+        names.append(MotionEvent.AXIS_GENERIC_13, "AXIS_GENERIC_13");
+        names.append(MotionEvent.AXIS_GENERIC_14, "AXIS_GENERIC_14");
+        names.append(MotionEvent.AXIS_GENERIC_15, "AXIS_GENERIC_15");
+        names.append(MotionEvent.AXIS_GENERIC_16, "AXIS_GENERIC_16");
+        
+        String axisName = names.get(axis);
+        if (null == axisName) {
+        	return "";
+        } else {
+        	return axisName;
+        }
+	}
+	
+	private void debugMotionEvent(MotionEvent motionEvent) {
+		float lsX = motionEvent.getAxisValue(OuyaController.AXIS_LS_X);
 	    float lsY = motionEvent.getAxisValue(OuyaController.AXIS_LS_Y);
 	    float rsX = motionEvent.getAxisValue(OuyaController.AXIS_RS_X);
 	    float rsY = motionEvent.getAxisValue(OuyaController.AXIS_RS_Y);
@@ -215,15 +275,13 @@ public class OuyaActivity extends Activity {
         	float val = motionEvent.getAxisValue(names.keyAt(i));
             Log.i(TAG, names.valueAt(i) + "=" + val);
         }
-		
-		return super.dispatchGenericMotionEvent(motionEvent);
 	}
 
 	@Override
 	public boolean dispatchKeyEvent(KeyEvent keyEvent) {
 		int keyCode = keyEvent.getKeyCode();
 		int source = keyEvent.getSource();
-		Log.i(TAG, android.os.Build.MODEL + " : dispatchKeyEvent="+keyCode);
+		Log.i(TAG, android.os.Build.MODEL + " : dispatchKeyEvent="+keyCode + " action="+keyEvent.getAction());
 		
 		/*
 		if (true) {
