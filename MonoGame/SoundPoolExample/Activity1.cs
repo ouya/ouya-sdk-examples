1a1dc6fc6caadb50493bca306aa39e0fda50ed57
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-07-09 15:27:56 -0700

    Updated sound pool example to use OE and have buttons for playing sounds

--- MonoGame/SoundPoolExample/Activity1.cs
+++ MonoGame/SoundPoolExample/Activity1.cs
@@ -1,6 +1,11 @@
+using System;
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Util;
+using Android.Views;
+using OuyaSdk;
 
 namespace SoundPoolExample
 {
@@ -12,15 +17,53 @@ namespace SoundPoolExample
         , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
         , ScreenOrientation = ScreenOrientation.SensorLandscape
         , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
-    public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
+    [IntentFilter(new[] { Intent.ActionMain }
+        , Categories = new[] { Intent.CategoryLauncher, OuyaIntent.CategoryGame })]
+    public class Activity1 : OuyaActivity
     {
+        private Game1 _game = null;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             Game1.Activity = this;
-            var g = new Game1();
-            SetContentView(g.Window);
-            g.Run();
+            _game = new Game1();
+            SetContentView(_game.Window);
+            _game.Run();
+        }
+
+        public override bool OnKeyUp(Keycode keyCode, KeyEvent keyEvent)
+        {
+            //Log.Info(TAG, "OnKeyUp=" + (int)keyCode);
+            int playerNum = OuyaController.getPlayerNumByDeviceId(keyEvent.DeviceId);
+            if (playerNum >= 0 && playerNum < 4)
+            {
+                if ((int)keyCode == OuyaController.BUTTON_DPAD_DOWN)
+                {
+                    _game.GetFocusManager().FocusDown();
+                }
+                if ((int)keyCode == OuyaController.BUTTON_DPAD_LEFT)
+                {
+                    _game.GetFocusManager().FocusLeft();
+                }
+                if ((int)keyCode == OuyaController.BUTTON_DPAD_RIGHT)
+                {
+                    _game.GetFocusManager().FocusRight();
+                }
+                if ((int)keyCode == OuyaController.BUTTON_DPAD_UP)
+                {
+                    _game.GetFocusManager().FocusUp();
+                }
+                if ((int)keyCode == OuyaController.BUTTON_O)
+                {
+                    _game.GetFocusManager().InvokeClick();
+                }
+                if ((int)keyCode == OuyaController.BUTTON_A)
+                {
+                    _game.Exit();
+                }
+            }
+            return true;
         }
     }
 }
--- MonoGame/SoundPoolExample/Game1.cs
+++ MonoGame/SoundPoolExample/Game1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Android.Content.Res;
 using Android.Media;
 using Microsoft.Xna.Framework;
@@ -12,25 +13,32 @@ namespace SoundPoolExample
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
-        SpriteFont font;
-        private SoundPool m_soundPool;
-        private int m_soundBoom;
-        private int m_soundOuch;
-        private DateTime m_timerSound = DateTime.MinValue;
-        private Random m_random = new Random(123);
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+        private SoundPool _soundPool;
+        private int _soundBoom;
+        private int _soundOuch;
+
+        private FocusManager _focusManager = new FocusManager();
+
+        private List<ButtonSprite> _buttons = new List<ButtonSprite>();
+
+        private ButtonSprite _btnBoom = null;
+        private ButtonSprite _btnOuch = null;
 
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
 
             Content.RootDirectory = "Content";
 
-            graphics.IsFullScreen = true;
-            graphics.PreferredBackBufferWidth = 1920;
-            graphics.PreferredBackBufferHeight = 1080;
-            graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+            _graphics.IsFullScreen = true;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft;
+
+            _focusManager.OnClick += OnClick;
         }
 
         /// <summary>
@@ -40,20 +48,69 @@ namespace SoundPoolExample
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            font = Content.Load<SpriteFont>("spriteFont1");
+            _font = Content.Load<SpriteFont>("spriteFont1");
 
-            m_soundPool = new SoundPool(5, Stream.Music, 0);
+            _soundPool = new SoundPool(5, Stream.Music, 0);
 
             AssetFileDescriptor assetFileDescriptor;
 
-            assetFileDescriptor = Activity.ApplicationContext.Assets.OpenFd("Content/ouch.wav");
-            m_soundBoom = m_soundPool.Load(assetFileDescriptor, 1);
-
             assetFileDescriptor = Activity.ApplicationContext.Assets.OpenFd("Content/boom.wav");
-            m_soundOuch = m_soundPool.Load(assetFileDescriptor, 1);
+            _soundBoom = _soundPool.Load(assetFileDescriptor, 1);
+
+            assetFileDescriptor = Activity.ApplicationContext.Assets.OpenFd("Content/ouch.wav");
+            _soundOuch = _soundPool.Load(assetFileDescriptor, 1);
+
+            _btnBoom = new ButtonSprite();
+            _btnBoom.Initialize(_font,
+                Content.Load<Texture2D>("Graphics\\ButtonActive"),
+                Content.Load<Texture2D>("Graphics\\ButtonInactive"));
+            _btnBoom.Position = new Vector2(150, 200);
+            _btnBoom.TextureScale = new Vector2(2f, 0.5f);
+            _btnBoom.Text = "Boom";
+            _btnBoom.TextOffset = new Vector2(40, 20);
+            _buttons.Add(_btnBoom);
+
+            _btnOuch = new ButtonSprite();
+            _btnOuch.Initialize(_font,
+                Content.Load<Texture2D>("Graphics\\ButtonActive"),
+                Content.Load<Texture2D>("Graphics\\ButtonInactive"));
+            _btnOuch.Position = new Vector2(600, 200);
+            _btnOuch.TextureScale = new Vector2(2f, 0.5f);
+            _btnOuch.Text = "Ouch";
+            _btnOuch.TextOffset = new Vector2(40, 20);
+            _buttons.Add(_btnOuch);
+
+            _focusManager.SelectedButton = _btnBoom;
+            _focusManager.Mappings[_btnBoom] = new FocusManager.ButtonMapping()
+            {
+                Right = _btnOuch
+            };
+            _focusManager.Mappings[_btnOuch] = new FocusManager.ButtonMapping()
+            {
+                Left = _btnBoom,
+            };
+        }
+
+        private void OnClick(object sender, FocusManager.ClickEventArgs clickEventArgs)
+        {
+            if (null == clickEventArgs ||
+                null == clickEventArgs.Button)
+            {
+                return;
+            }
+
+            if (clickEventArgs.Button == _btnBoom)
+            {
+                _soundPool.Play(_soundBoom, 0.99f, 0.99f, 0, 0, 1);   
+            }
+
+            if (clickEventArgs.Button == _btnOuch)
+            {
+                _soundPool.Play(_soundOuch, 0.99f, 0.99f, 0, 0, 1);
+            }
         }
 
         /// <summary>
@@ -63,23 +120,15 @@ namespace SoundPoolExample
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            foreach (ButtonSprite button in _buttons)
             {
-                Exit();
-            }
-
-            if (m_timerSound < DateTime.Now)
-            {
-                m_timerSound = DateTime.Now + TimeSpan.FromSeconds(1);
-
-                switch (m_random.Next(0, 2))
+                if (button == _focusManager.SelectedButton)
                 {
-                    case 0:
-                        m_soundPool.Play(m_soundBoom, 0.99f, 0.99f, 0, 0, 1);
-                        break;
-                    case 1:
-                        m_soundPool.Play(m_soundOuch, 0.99f, 0.99f, 0, 0, 1);
-                        break;
+                    button.ButtonTexture = button.ButtonActive;
+                }
+                else
+                {
+                    button.ButtonTexture = button.ButtonInactive;
                 }
             }
 
@@ -92,13 +141,23 @@ namespace SoundPoolExample
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+            _graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            spriteBatch.Begin();
-            spriteBatch.DrawString(font, "MonoGame SoundPool example!", new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2), Color.White);
-            spriteBatch.End();
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_font, "MonoGame SoundPool example!", 
+                new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2), Color.White);
+            foreach (ButtonSprite button in _buttons)
+            {
+                button.Draw(_spriteBatch);
+            }
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        public FocusManager GetFocusManager()
+        {
+            return _focusManager;
         }
     }
 }
