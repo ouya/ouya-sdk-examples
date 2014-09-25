a58b89b19fe1df518d58ba0c6f48cc765eae704e
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-09-25 10:22:58 -0700

    Added Xamarin example project for adding an OuyaView to the content view.

---
+++ Xamarin/InputView/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using OuyaSdk;
+using System;
+
+namespace InputView
+{
+	[Activity (Label = "InputView", MainLauncher = true)]
+	[IntentFilter(new[] { Intent.ActionMain }
+		, Categories = new[] { Intent.CategoryLauncher, CategoryGame })]
+	public class MainActivity : Activity
+	{
+		public const String CategoryGame = "tv.ouya.intent.category.GAME";
+
+		private OuyaInputView m_inputView = null;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			m_inputView = new OuyaInputView(Handle, ApplicationContext.Handle);
+		}
+	}
+}
+
+
