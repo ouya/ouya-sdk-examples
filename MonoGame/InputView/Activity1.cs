59d79dbe1a75daa0a78a527cace48ab14c5bbf2d
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-09-25 15:31:38 -0700

    Setup an InputView project for MonoGame

---
+++ MonoGame/InputView/Activity1.cs
@@ -0,0 +1,39 @@
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using System;
+
+namespace InputView
+{
+    [Activity(Label = "MonoGame InputView"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = Android.Content.PM.LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.SensorLandscape
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden)]
+    [IntentFilter(new[] { Intent.ActionMain }
+        , Categories = new[] { Intent.CategoryLauncher, CategoryGame })]
+    public class Activity1 : Microsoft.Xna.Framework.AndroidGameActivity
+    {
+        public const String CategoryGame = "tv.ouya.intent.category.GAME";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            using (var ignore = new TV.Ouya.Sdk.OuyaInputView(this, ApplicationContext))
+            {
+                // do nothing
+            }
+
+            Game1.Activity = this;
+            var g = new Game1();
+            SetContentView(g.Window);
+            g.Run();
+        }
+    }
+}
+
