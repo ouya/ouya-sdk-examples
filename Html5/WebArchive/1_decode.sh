8636e1983f4daf48d367b5bd50480f1832032280
tgraupmann <tgraupmann@gmail.com>
2014-10-02 12:19:53 -0700

    Adding Mac scripts

---
+++ Html5/WebArchive/1_decode.sh
@@ -0,0 +1,4 @@
+. ./init.sh
+echo APKTOOL=$APKTOOL
+java -jar $APKTOOL decode ContentShell.apk
+#pause
---
+++ Html5/WebArchive/init.sh
@@ -0,0 +1,4 @@
+# apk tool requires JDK 7 atm
+#export JDK7=c:\Program Files\Java\jdk1.7.0_67
+
+export APKTOOL=./apktool_2.0.0b7.jar
