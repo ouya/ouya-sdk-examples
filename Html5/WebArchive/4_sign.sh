1fbbb7039f4483a4731562b19cb07a5bc24e5b34
tgraupmann <tgraupmann@gmail.com>
2014-11-07 16:56:13 -0800

    Upgraded to ODK 2.0.1353

--- Html5/WebArchive/4_sign.sh
+++ Html5/WebArchive/4_sign.sh
@@ -1,5 +1,7 @@
 rm ContentShellSigned.apk
 cp ContentShellAligned.apk ContentShellSigned.apk
-jarsigner -tsa http://timestamp.digicert.com -keystore debug.keystore -storepass android -keypass android ContentShellSigned.apk androiddebugkey
-jarsigner -verify -certs -verbose ContentShellSigned.apk
+export JARSIGNER=/Library/Java/JavaVirtualMachines/1.6.0_51-b11-457.jdk/Contents/Home/bin/jarsigner
+#export JARSIGNER=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home/bin/jarsigner
+$JARSIGNER -tsa http://timestamp.digicert.com -keystore ./debug.keystore -storepass android -keypass android ContentShellSigned.apk androiddebugkey
+$JARSIGNER -verify -certs -verbose ContentShellSigned.apk
 sleep 1
