fc20950c12a93d5f243cce0029331034a4e38a75
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-11-07 19:13:26 -0800

    Customize icon and auto package

--- Html5/VirtualController/package.cmd
+++ Html5/VirtualController/package.cmd
@@ -1,4 +1,8 @@
 copy /Y customization\AndroidManifest.xml ..\WebArchive\ContentShell\AndroidManifest.xml
+copy /Y customization\res\drawable\app_icon.png ..\WebArchive\ContentShell\res\drawable\app_icon.png
+copy /Y customization\res\drawable\splash_screen.png ..\WebArchive\ContentShell\res\drawable\splash_screen.png
+copy /Y customization\res\drawable-xhdpi\ouya_icon.png ..\WebArchive\ContentShell\res\drawable-xhdpi\ouya_icon.png
+copy /Y customization\res\values\strings.xml ..\WebArchive\ContentShell\res\values\strings.xml
 cd web_archive
 zip ../web_archive.jar *
 move ..\web_archive.jar ..\..\WebArchive\ContentShell\res\raw\web_archive.jar
