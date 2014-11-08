87ae77a5a1aba7d9dd4ff759048ffd7380f2fea8
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-11-07 19:24:51 -0800

    Customized icon for in-app-purchases example

--- Html5/InAppPurchases/package.cmd
+++ Html5/InAppPurchases/package.cmd
@@ -1,3 +1,9 @@
+copy /Y customization\AndroidManifest.xml ..\WebArchive\ContentShell\AndroidManifest.xml
+copy /Y customization\assets\key.der ..\WebArchive\ContentShell\assets\key.der
+copy /Y customization\res\drawable\app_icon.png ..\WebArchive\ContentShell\res\drawable\app_icon.png
+copy /Y customization\res\drawable\splash_screen.png ..\WebArchive\ContentShell\res\drawable\splash_screen.png
+copy /Y customization\res\drawable-xhdpi\ouya_icon.png ..\WebArchive\ContentShell\res\drawable-xhdpi\ouya_icon.png
+copy /Y customization\res\values\strings.xml ..\WebArchive\ContentShell\res\values\strings.xml
 cd web_archive
 zip ../web_archive.jar *
 move ..\web_archive.jar ..\..\WebArchive\ContentShell\res\raw\web_archive.jar
