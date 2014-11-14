a2a9a44a270ace974d0d86af51713f01df507e6f
Timothy A. Graupmann <tgraupmann@gmail.com>
2014-11-13 16:29:54 -0800

    Upgraded to TADP 3.0r4 - https://developer.nvidia.com/gameworksdownload#?dn=tegra-android-development-pack-3-0r4

--- Android/HelloWorldNative/jni/HelloJni.cpp
+++ Android/HelloWorldNative/jni/HelloJni.cpp
@@ -5,9 +5,9 @@
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
  */
+extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
-                                                  jobject thiz )
+Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env, jobject thiz )
 {
-    return (*env)->NewStringUTF(env, "Hello World");
+    return env->NewStringUTF("Hello World");
 }
