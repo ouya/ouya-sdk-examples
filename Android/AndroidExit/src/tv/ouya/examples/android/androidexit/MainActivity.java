3bb209f8efa6e510be7ec7adff01dea216322867
Timothy A. Graupmann <tgraupmann@gmail.com>
2015-02-19 09:08:53 -0800

    Log the localized strings

--- Android/AndroidExit/src/tv/ouya/examples/android/androidexit/MainActivity.java
+++ Android/AndroidExit/src/tv/ouya/examples/android/androidexit/MainActivity.java
@@ -2,12 +2,23 @@ package tv.ouya.examples.android.androidexit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
+	private static final String TAG = MainActivity.class.getSimpleName();
+	
 	Button m_btnExit = null;
+	
+	private String getStringResource(String name) {
+		int id = getResources().getIdentifier(name, "string", getPackageName());
+		if (id <= 0) {
+			return "";
+		}
+		return getResources().getString(id);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +33,8 @@ public class MainActivity extends Activity {
 					finish();
 				}
 			});
+		
+		Log.i(TAG, "Localized string: app_name="+getStringResource("app_name"));
+		Log.i(TAG, "Localized string: exit="+getStringResource("exit"));
 	}
 }
