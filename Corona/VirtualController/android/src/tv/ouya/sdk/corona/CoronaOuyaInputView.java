34db03e7c3601e56990389ab75a7c53ad9b2d28c
Tim Graupmann <tgraupmann@gmail.com>
2015-08-05 10:04:49 -0700

    Added ForgeTV support

--- Corona/VirtualController/android/src/tv/ouya/sdk/corona/CoronaOuyaInputView.java
+++ Corona/VirtualController/android/src/tv/ouya/sdk/corona/CoronaOuyaInputView.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import tv.ouya.console.api.*;
+import tv.ouya.sdk.DebugInput;
 
 public class CoronaOuyaInputView extends View {
 	
