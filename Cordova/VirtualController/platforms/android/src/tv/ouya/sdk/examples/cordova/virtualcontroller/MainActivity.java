c169060b6a2db0114ee837f10b2d608fa01c3618
Timothy A. Graupmann <tgraupmann@gmail.com>
2015-08-06 18:09:58 -0700

    Added Forge TV support

--- Cordova/VirtualController/platforms/android/src/tv/ouya/sdk/examples/cordova/virtualcontroller/MainActivity.java
+++ Cordova/VirtualController/platforms/android/src/tv/ouya/sdk/examples/cordova/virtualcontroller/MainActivity.java
@@ -20,35 +20,111 @@
 package tv.ouya.sdk.examples.cordova.virtualcontroller;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
+import android.view.InputDevice;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import java.util.HashMap;
 import org.apache.cordova.*;
+
 import tv.ouya.console.api.OuyaController;
+import tv.ouya.sdk.OuyaInputView;
 
 public class MainActivity extends CordovaActivity
 {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static SparseArray<HashMap<Integer, Float>> sAxisValues = new SparseArray<HashMap<Integer, Float>>();
-    private static SparseArray<HashMap<Integer, Boolean>> sButtonValues = new SparseArray<HashMap<Integer, Boolean>>();
+    private static final boolean sEnableLogging = false;
 
-    static {
-        for (int index = 0; index < OuyaController.MAX_CONTROLLERS; ++index) {
-            HashMap<Integer, Float> axisMap = new HashMap<Integer, Float>();
-            axisMap.put(MotionEvent.AXIS_HAT_X, 0f);
-            axisMap.put(MotionEvent.AXIS_HAT_Y, 0f);
-            sAxisValues.put(index, axisMap);
-            HashMap<Integer, Boolean> buttonMap = new HashMap<Integer, Boolean>();
-            sButtonValues.put(index, buttonMap);
-        }
-    }
+    OuyaInputView mInputView = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        mInputView = new OuyaInputView(this);
+
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
+    }
+
+    @Override
+    public void onDestroy ()
+    {
+        super.onDestroy();
+        if (null != mInputView) {
+            mInputView.shutdown();
+        }
+    }
+
+    @Override
+    public boolean dispatchGenericMotionEvent(MotionEvent motionEvent) {
+        if (sEnableLogging) {
+            Log.i(TAG, "dispatchGenericMotionEvent");
+        }
+        if (null != mInputView) {
+            mInputView.dispatchGenericMotionEvent(motionEvent);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent keyEvent) {
+        if (sEnableLogging) {
+            Log.i(TAG, "dispatchKeyEvent keyCode="+keyEvent.getKeyCode());
+        }
+        InputDevice device = keyEvent.getDevice();
+        if (null != device) {
+            String name = device.getName();
+            if (null != name &&
+                    name.equals("aml_keypad")) {
+                switch (keyEvent.getKeyCode()) {
+                    case 24:
+                        if (sEnableLogging) {
+                            Log.i(TAG, "Volume Up detected.");
+                        }
+                        //raiseVolume();
+                        //return true; //the volume was handled
+                        return false; //show the xiaomi volume overlay
+                    case 25:
+                        if (sEnableLogging) {
+                            Log.i(TAG, "Volume Down detected.");
+                        }
+                        //lowerVolume();
+                        //return true; //the volume was handled
+                        return false; //show the xiaomi volume overlay
+                    case 66:
+                        if (sEnableLogging) {
+                            Log.i(TAG, "Remote button detected.");
+                        }
+                        if (null != mInputView) {
+                            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                                mInputView.onKeyDown(OuyaController.BUTTON_O, keyEvent);
+                            } else if (keyEvent.getAction() == KeyEvent.ACTION_UP) {
+                                mInputView.onKeyUp(OuyaController.BUTTON_O, keyEvent);
+                            }
+                        }
+                        return false;
+                    case 4:
+                        if (sEnableLogging) {
+                            Log.i(TAG, "Remote back button detected.");
+                        }
+                        if (null != mInputView) {
+                            if (keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+                                mInputView.onKeyDown(OuyaController.BUTTON_A, keyEvent);
+                            } else if (keyEvent.getAction() == KeyEvent.ACTION_UP) {
+                                mInputView.onKeyUp(OuyaController.BUTTON_A, keyEvent);
+                            }
+                        }
+                        return true;
+                }
+            }
+        }
+        if (null != mInputView) {
+            mInputView.dispatchKeyEvent(keyEvent);
+        }
+        return true;
     }
 }
