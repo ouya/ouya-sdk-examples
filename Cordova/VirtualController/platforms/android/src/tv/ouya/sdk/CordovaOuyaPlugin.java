4e2a37f8eae823d4344f8f3c3ca945c2edf67155
Timothy A. Graupmann <tgraupmann@gmail.com>
2015-08-07 14:37:05 -0700

    Renamed package location added plugin hooks

---
+++ Cordova/VirtualController/platforms/android/src/tv/ouya/sdk/CordovaOuyaPlugin.java
@@ -0,0 +1,43 @@
+package tv.ouya.sdk;
+
+import android.util.Log;
+
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class CordovaOuyaPlugin extends CordovaPlugin {
+
+    private static final String TAG = CordovaOuyaPlugin.class.getSimpleName();
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        Log.i(TAG, "********************* Initialize plugin");
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("echo")) {
+            String message = args.getString(0);
+            this.echo(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
---
+++ Cordova/VirtualController/run.cmd
@@ -0,0 +1,1 @@
+adb shell am start -n tv.ouya.sdk.examples.cordova.virtualcontroller/.MainActivity
