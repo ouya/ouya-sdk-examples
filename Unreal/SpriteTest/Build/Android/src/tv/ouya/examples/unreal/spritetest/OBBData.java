468b3cab69c7a23d4f091233c2b156784f267e2b
Timothy A. Graupmann <tgraupmann@gmail.com>
2015-08-25 22:09:01 -0700

    Updated to API 16.

--- Unreal/SpriteTest/Build/Android/src/tv/ouya/examples/unreal/spritetest/OBBData.java
+++ Unreal/SpriteTest/Build/Android/src/tv/ouya/examples/unreal/spritetest/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-35598153 // the length of the file in bytes
+39482709 // the length of the file in bytes
 )
 };
 };
