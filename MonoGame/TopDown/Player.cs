d78ce5a6e43f1becd3b5c426bf2092bb98aa9f0f
Tim Graupmann <tgraupmann@gmail.com>
2015-10-21 17:59:21 -0700

    Added a light texture for the player

--- MonoGame/TopDown/Player.cs
+++ MonoGame/TopDown/Player.cs
@@ -44,6 +44,11 @@ namespace TopDown
         Texture2D _mTexture = null;
 
         /// <summary>
+        /// Light of the player
+        /// </summary>
+        Texture2D _mLight = null;
+
+        /// <summary>
         /// The projectile prefab fired by the player
         /// </summary>
         Projectile _mProjectilePrefab = null;
@@ -68,6 +73,83 @@ namespace TopDown
         }
 
         /// <summary>
+        /// Create a circle texture
+        /// Ref: http://stackoverflow.com/questions/5641579/xna-draw-a-filled-circle
+        /// </summary>
+        /// <param name="importedGraphicsDevice"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        private Texture2D CreateCircle(GraphicsDevice graphicsDevice, int radius)
+        {
+            int outerRadius = radius * 2 + 2; // So circle doesn't go out of bounds
+            Texture2D texture = new Texture2D(graphicsDevice, outerRadius, outerRadius);
+
+            Color[] data = new Color[outerRadius * outerRadius];
+
+            // Colour the entire texture transparent first.
+            for (int i = 0; i < data.Length; i++)
+                data[i] = Color.Transparent;
+
+            // Work out the minimum step necessary using trigonometry + sine approximation.
+            double angleStep = 1f / radius;
+
+            for (double angle = 0; angle < Math.PI * 2; angle += angleStep)
+            {
+                // Use the parametric definition of a circle: http://en.wikipedia.org/wiki/Circle#Cartesian_coordinates
+                int x = (int)Math.Round(radius + radius * Math.Cos(angle));
+                int y = (int)Math.Round(radius + radius * Math.Sin(angle));
+
+                data[y * outerRadius + x + 1] = Color.White;
+            }
+
+            //width
+            for (int i = 0; i < outerRadius; i++)
+            {
+                int yStart = -1;
+                int yEnd = -1;
+
+                //loop through height to find start and end to fill
+                for (int j = 0; j < outerRadius; j++)
+                {
+
+                    if (yStart == -1)
+                    {
+                        if (j == outerRadius - 1)
+                        {
+                            //last row so there is no row below to compare to
+                            break;
+                        }
+
+                        //start is indicated by Color followed by Transparent
+                        if (data[i + (j * outerRadius)] == Color.White && data[i + ((j + 1) * outerRadius)] == Color.Transparent)
+                        {
+                            yStart = j + 1;
+                            continue;
+                        }
+                    }
+                    else if (data[i + (j * outerRadius)] == Color.White)
+                    {
+                        yEnd = j;
+                        break;
+                    }
+                }
+
+                //if we found a valid start and end position
+                if (yStart != -1 && yEnd != -1)
+                {
+                    //height
+                    for (int j = yStart; j < yEnd; j++)
+                    {
+                        data[i + (j * outerRadius)] = Color.White;
+                    }
+                }
+            }
+
+            texture.SetData(data);
+            return texture;
+        }
+
+        /// <summary>
         /// Create the texture for the player
         /// </summary>
         /// <param name="graphicsDevice"></param>
@@ -81,6 +163,8 @@ namespace TopDown
             }
             _mTexture.SetData(data);
 
+            _mLight = CreateCircle(graphicsDevice, (int)(_mRectangle.Width*0.75f));
+
             _mProjectilePrefab.LoadContent(graphicsDevice);
         }
 
@@ -92,6 +176,7 @@ namespace TopDown
         /// <param name="gameTime"></param>
         public void Draw(SpriteBatch spriteBatch, SpriteFont font, GameTime gameTime)
         {
+            spriteBatch.Draw(_mLight, _mPosition - new Vector2(_mRectangle.Width * 0.25f, _mRectangle.Height * 0.25f), Color.White);
             spriteBatch.Draw(_mTexture, _mPosition, Color.White);
             spriteBatch.DrawString(font, string.Format("{0}", _mIndex + 1),
                 _mPosition + new Vector2(_mRectangle.Width / 2 - 5, _mRectangle.Height / 2 - 10),
