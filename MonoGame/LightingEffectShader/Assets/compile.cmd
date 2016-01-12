210048e4b4218253ffd970c0d8058a1cad11c0d2
Timothy Graupmann <tim@tagenigma.com>
2016-01-11 16:25:46 -0800

    Updated path

--- MonoGame/LightingEffectShader/Assets/compile.cmd
+++ MonoGame/LightingEffectShader/Assets/compile.cmd
@@ -1,2 +1,3 @@
-"C:\Users\tgraupmann\Documents\TAGENIGMA\Ouya\Public\MonoGame\Tools\2MGFX\bin\Windows\AnyCPU\Debug\2MGFX.exe" LightingEffect.fx LightingEffectOuya.xnb
-pause
+REM HAVE 2MGFX.exe IN YOUR PATH
+2MGFX.exe LightingEffect.fx LightingEffectOuya.xnb
+PAUSE
