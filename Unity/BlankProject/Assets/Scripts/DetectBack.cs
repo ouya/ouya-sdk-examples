6d9d4a2674168f54be7165b145d1e19c4d1e75db
Tim Graupmann <tim@tagenigma.com>
2016-09-19 13:32:48 -0700

    Added example to detect the back button

---
+++ Unity/BlankProject/Assets/Scripts/DetectBack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class DetectBack : MonoBehaviour
+{
+    private DateTime _mBackDetected = DateTime.MinValue;
+
+    void OnGUI()
+    {
+        if (_mBackDetected != DateTime.MinValue)
+        {
+            GUILayout.BeginVertical(GUILayout.Height(Screen.height));
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("Back detected");
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _mBackDetected = DateTime.Now + TimeSpan.FromSeconds(1);
+        }
+        else if (_mBackDetected != DateTime.Now &&
+            _mBackDetected < DateTime.Now)
+        {
+            _mBackDetected = DateTime.MinValue;
+        }
+    }
+}
