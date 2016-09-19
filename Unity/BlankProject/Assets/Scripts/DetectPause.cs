854422922fa2c8700d13c9c4b6ecfed756b56355
Tim Graupmann <tim@tagenigma.com>
2016-09-19 13:57:12 -0700

    Added sample for detecting pause

---
+++ Unity/BlankProject/Assets/Scripts/DetectPause.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+public class DetectPause : MonoBehaviour
+{
+    private DateTime _mPauseDetected = DateTime.MinValue;
+
+    void OnGUI()
+    {
+        if (_mPauseDetected != DateTime.MinValue)
+        {
+            GUILayout.BeginVertical(GUILayout.Height(Screen.height)); 
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal(GUILayout.Width(Screen.width));
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("");
+            GUILayout.Label("PAUSE DETECTED");
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+        }
+    }
+
+    void OnApplicationFocus(bool focusStatus)
+    {
+        if (!focusStatus)
+        {
+            _mPauseDetected = DateTime.Now + TimeSpan.FromSeconds(1);
+        }
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        _mPauseDetected = DateTime.Now + TimeSpan.FromSeconds(1);
+    }
+
+    void Update()
+    {
+        if (_mPauseDetected != DateTime.Now &&
+            _mPauseDetected < DateTime.Now)
+        {
+            _mPauseDetected = DateTime.MinValue;
+        }
+    }
+}
